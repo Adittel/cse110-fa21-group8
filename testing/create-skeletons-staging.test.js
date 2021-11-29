6cd111f8ea0e45200cff788abf47d3b68f0b82a6
Adittel <telang.adithya@gmail.com>
2021-11-28 21:16:14 -0800

    Co-authored-by: JimLiang610 <JimLiang610@users.noreply.github.com>

--- testing/create-skeletons-staging.test.js
+++ testing/create-skeletons-staging.test.js
@@ -2,7 +2,7 @@ describe("Basic user flow for Website", () => {
   // First, visit the website
   beforeAll(async () => {
     await page.goto(
-      "https://testing.tianyuezhang1997.site/cse110-fa21-group8/create-skeletons-staging/source/home-page"
+      "https://testing.tianyuezhang1997.site/cse110-fa21-group8/create-skeletons-staging/source/"
     );
   });
 
@@ -16,4 +16,22 @@ describe("Basic user flow for Website", () => {
     }
     expect(message.type()).not.toBe("error");
   });
+
+  it("Check ingredients checkbox can be clicked", async () => {
+    //click ingredient 1
+    let button1 = await page.$("#ingredient1");
+    await button1.click();
+    //click ingredient 2
+    let button2 = await page.$("#ingredient2");
+    await button2.click();
+    //click ingredient 3
+    let button3 = await page.$("#ingredient3");
+    await button3.click();
+
+    expect(button1.checked).toBe(true);
+    expect(button1.checked).toBe(true);
+    expect(button1.checked).toBe(true);
+  });
+
+
 });
